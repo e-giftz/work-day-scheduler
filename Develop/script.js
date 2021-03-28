2bc629857a93e921a3e507314deb8fe4a2345c6f
Gift <56089461+e-giftz@users.noreply.github.com>
2021-03-28 01:23:37 -0400

    Add function to track time

--- Develop/script.js
+++ Develop/script.js
@@ -9,84 +9,85 @@ currentDayEl.text(today);
 // When the save button is clicked
 function saveEntry (event) {
     event.preventDefault();
-    //alert('Hello World');
 
-    //var calendarEntry = textEntryEl.val();
     var calendarEntry = $(this).siblings('.calendar-item').val();
     var timeEntry = $(this).parent().attr("id");
-    console.log(calendarEntry);
   
     localStorage.setItem(timeEntry, calendarEntry);
 }
 
 function loadEntry () {
-    console.log('hello');
-    console.log(localStorage['hour-9']);
-    if (localStorage['hour-9'] !== null) {
+
+    if (localStorage['hour-9'] !== null && localStorage['hour-9'] !== undefined) {
         var entryNineAm= $('<p>' + localStorage['hour-9'] + '</p>');
-        console.log(entryNineAm);
         $("#nineAm").append(entryNineAm[0].innerText);
     }else {
         ("");
     }
-    if (localStorage['hour-10'] !== null) {
+    if (localStorage['hour-10'] !== null && localStorage['hour-10'] !== undefined) {
         var entryTenAm= $('<p>' + localStorage['hour-10'] + '</p>');
-        console.log(entryTenAm);
         $("#tenAm").append(entryTenAm[0].innerText);
     }else {
         ("");
     }
-    if (localStorage['hour-11'] !== null) {
+    if (localStorage['hour-11'] !== null && localStorage['hour-11'] !== undefined) {
         var entryElevenAm= $('<p>' + localStorage['hour-11'] + '</p>');
-        console.log(entryElevenAm);
         $("#elevenAm").append(entryElevenAm[0].innerText);
     }else {
         ("");
     }
-    if (localStorage['hour-12'] !== null) {
+    if (localStorage['hour-12'] !== null && localStorage['hour-12'] !== undefined) {
         var entryTwelvePm= $('<p>' + localStorage['hour-12'] + '</p>');
-        console.log(entryTwelvePm);
         $("#twelvePm").append(entryTwelvePm[0].innerText);
     }else {
         ("");
     }
-    if (localStorage['hour-13'] !== null) {
+    if (localStorage['hour-13'] !== null && localStorage['hour-13'] !== undefined) {
         var entryOnePm= $('<p>' + localStorage['hour-13'] + '</p>');
-        console.log(entryOnePm);
         $("#onePm").append(entryOnePm[0].innerText);
     }else {
         ("");
     }
-    if (localStorage['hour-14'] !== null) {
+    if (localStorage['hour-14'] !== null && localStorage['hour-14'] !== undefined) {
         var entryTwoPm= $('<p>' + localStorage['hour-14'] + '</p>');
-        console.log(entryTwoPm);
         $("#twoPm").append(entryTwoPm[0].innerText);
     }else {
         ("");
     }
-    if (localStorage['hour-15'] !== null) {
+    if (localStorage['hour-15'] !== null && localStorage['hour-15'] !== undefined) {
         var entryThreePm= $('<p>' + localStorage['hour-15'] + '</p>');
-        console.log(entryThreePm);
         $("#threePm").append(entryThreePm[0].innerText);
     }else {
         ("");
     }
-    if (localStorage['hour-16'] !== null) {
+    if (localStorage['hour-16'] !== null && localStorage['hour-16'] !== undefined) {
         var entryFourPm= $('<p>' + localStorage['hour-16'] + '</p>');
-        console.log(entryFourPm);
         $("#fourPm").append(entryFourPm[0].innerText);
     }else {
         ("");
     }
-    if (localStorage['hour-17'] !== null) {
+    if (localStorage['hour-17'] !== null && localStorage['hour-17'] !== undefined) {
         var entryFivePm= $('<p>' + localStorage['hour-17'] + '</p>');
-        console.log(entryFivePm);
         $("#fivePm").append(entryFivePm[0].innerText);
     }else {
         ("");
     }
 };
-loadEntry();
+// Create funtion to track time
+function trackTime () {
+    var now = moment().hour();
+    $(".row").each(function () {
+        var blockTime = parseInt($(this).attr("id"));
+        if (blockTime === now) {
+            $(this).addClass("present");
+        }else if (blockTime < now) {
+            $(this).addClass("past");
+        }else {
+            $(this).addClass("future");
+        }
+    })
+}
+
 buttonSaveEl.on('click', saveEntry);
-
-
+loadEntry();
+trackTime();
